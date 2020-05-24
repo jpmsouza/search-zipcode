187702d382167b246c57773f5d7754b78eac94cd
João Pedro Martins Souza <joao.souza@gec.inatel.br>
2020-05-24 12:04:04 -0300

    Initializing project

---
+++ src/main/java/com/api/searchzipcode/SearchZipcodeApplication.java
@@ -0,0 +1,13 @@
+package com.api.searchzipcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchZipcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchZipcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/searchzipcode/SearchZipcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.searchzipcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchZipcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
