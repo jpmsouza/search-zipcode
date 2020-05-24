10aff8581f98b8264e1a60435cd66bea52ef20ce
João Pedro Martins Souza <joao.souza@gec.inatel.br>
2020-05-24 16:17:55 -0300

    Create DB struct and class for mock data using H2
    It was created a file schema.sql to create the databe schema and a file
    data.sql to simulate the insert of data into this h2 db.
    It was created a DataSourceConfig class to setup and pont the domains
    and repositories packages.
    The application.yml file ws modified to setup the use of h2 database and
    datasource.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,83 @@
+INSERT INTO ZIP_CODE(NUM_POSTAL_CODE,
+                    STR_STREET,
+                    STR_CITY,
+                    STR_STATE,
+                    STR_NEIGHBORHOOD,
+                    STR_IBGECODE) values
+                    (12230087,
+                     'Rua Moises Tristão dos Santos',
+                     'São José dos Campos',
+                     'SP',
+                     'Floradas de São José',
+                     3549904
+                    );
+
+INSERT INTO ZIP_CODE(NUM_POSTAL_CODE,
+                    STR_STREET,
+                    STR_CITY,
+                    STR_STATE,
+                    STR_NEIGHBORHOOD,
+                    STR_IBGECODE) values
+                    (12209071,
+                     'Rua Ana Eufrásia',
+                     'São José dos Campos',
+                     'SP',
+                     'Jardim Bela Vista',
+                     3549904
+                    );
+
+INSERT INTO ZIP_CODE(NUM_POSTAL_CODE,
+                    STR_STREET,
+                    STR_CITY,
+                    STR_STATE,
+                    STR_NEIGHBORHOOD,
+                    STR_IBGECODE) values
+                    (01504001,
+                     'Rua Vergueiro',
+                     'São Paulo',
+                     'SP',
+                     'Liberdade',
+                     3550308
+                    );
+
+INSERT INTO ZIP_CODE(NUM_POSTAL_CODE,
+                    STR_STREET,
+                    STR_CITY,
+                    STR_STATE,
+                    STR_NEIGHBORHOOD,
+                    STR_IBGECODE) values
+                    (03047000,
+                     'Rua 21 de Abril',
+                     'São Paulo',
+                     'SP',
+                     'Brás',
+                     3550308
+                    );
+
+INSERT INTO ZIP_CODE(NUM_POSTAL_CODE,
+                    STR_STREET,
+                    STR_CITY,
+                    STR_STATE,
+                    STR_NEIGHBORHOOD,
+                    STR_IBGECODE) values
+                    (37582000,
+                     '',
+                     'Careaçu',
+                     'MG',
+                     '',
+                     37582000
+                    );
+
+INSERT INTO ZIP_CODE(NUM_POSTAL_CODE,
+                    STR_STREET,
+                    STR_CITY,
+                    STR_STATE,
+                    STR_NEIGHBORHOOD,
+                    STR_IBGECODE) values
+                    (37490000,
+                     '',
+                     'São Gonçalo do Sapucaí',
+                     'MG',
+                     '',
+                     3162005
+                    );
