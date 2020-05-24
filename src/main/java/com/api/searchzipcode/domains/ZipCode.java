c6dbe4b54c89c09061f403ed83f976edeec7914d
João Pedro Martins Souza <joao.souza@gec.inatel.br>
2020-05-24 16:44:02 -0300

    Created the class struct of entity ZipCode
    It was created the attributes of entity ZipCode and related with
    its respective columns in the h2 database.

--- src/main/java/com/api/searchzipcode/domains/ZipCode.java
+++ src/main/java/com/api/searchzipcode/domains/ZipCode.java
@@ -1,4 +1,35 @@
 package com.api.searchzipcode.domains;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ZIP_CODE")
 public class ZipCode {
+
+    @Id
+    @Column(name = "NUM_POSTAL_CODE")
+    private Integer postalCode;
+
+    @Column(name = "STR_STREET")
+    private String street;
+
+    @Column(name = "STR_CITY")
+    private String city;
+
+    @Column(name = "STR_STATE")
+    private String state;
+
+    @Column(name = "STR_NEIGHBORHOOD")
+    private String neighborhood;
+
+    @Column(name = "STR_IBGECODE")
+    private Integer ibgeCode;
 }
